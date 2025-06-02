a81e3c5af256ced1b112ed729f2e45b6bdb7fbb5
CarlossViniciuss <carlosvinicius.index@gmail.com>
2025-06-02 15:38:19 -0300

    Removendo comentarios

--- run_let.py
+++ run_let.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
-
 import sys
 import os
 
-# Adiciona o diretório atual ao path
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Importa o CLI e executa o comando
 from cli import cli
 
 if __name__ == "__main__":
-    cli()  # Executa o CLI diretamente, sem argumentos
+    cli() 
--- test_let.py
+++ test_let.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python
-
 import sys
 import os
 
-# Adiciona o diretório atual ao path
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Importa o módulo let
 try:
     import let.cli
     print("Módulo let importado com sucesso!")
